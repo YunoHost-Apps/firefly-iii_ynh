0143c97cb76fd92a4fbc50d7a00e2516ce2334cb
ericgaspar <junk.eg@free.fr>
2021-06-04 13:30:51 +0200

    Remove composer helper

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,6 +15,9 @@ YNH_PHP_VERSION="7.4"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-opcache php${YNH_PHP_VERSION}-ldap"
 
+# Composer version
+YNH_COMPOSER_VERSION="2.0.13"
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================

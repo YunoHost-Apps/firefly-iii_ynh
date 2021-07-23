fe5a4131da21f38edcf8e3c1d9f8594898676491
ericgaspar <junk.eg@free.fr>
2021-07-23 14:55:35 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,9 @@ pkg_dependencies="redis-server"
 
 YNH_PHP_VERSION="7.4"
 
+# Composer version
+YNH_COMPOSER_VERSION="2.0.13"
+
 extra_php_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-opcache php${YNH_PHP_VERSION}-ldap"
 
 #=================================================

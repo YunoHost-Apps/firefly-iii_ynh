76fe6d0ad5496e55ee767112939cbbf7b5f23b30
Memo <memo.froese@gmail.com>
2022-04-07 15:56:25 +0200

    Revert "Update _common.sh"
    This reverts commit a4884862b06475c4e0bb6057776743afe0aeee8f.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@
 YNH_PHP_VERSION="8.0"
 
 # Composer version
-YNH_COMPOSER_VERSION="2.3.3"
+YNH_COMPOSER_VERSION="2.0.13"
 
 pkg_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-opcache php${YNH_PHP_VERSION}-ldap"
 
